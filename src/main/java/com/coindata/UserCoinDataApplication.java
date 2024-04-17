07e6e212abc1c15fa46c0eef8365ecb5e8473d8f
bhumika-aga <bhumika.aga@gmail.com>
2024-04-17 11:38:57 +0530

    Initial Project Setup

---
+++ src/main/java/com/coindata/UserCoinDataApplication.java
@@ -0,0 +1,13 @@
+package com.coindata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCoinDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCoinDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coindata/UserCoinDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coindata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCoinDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
